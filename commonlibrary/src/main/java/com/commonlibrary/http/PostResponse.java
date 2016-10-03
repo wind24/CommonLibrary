a0f8df9ba6eea651d89097c3d405b2b9bba8ccf8
hzf <huangzefeng30@gmail.com>
2016-10-03 13:07:30 +0800

    PostResponse增加返回结果类型

--- commonlibrary/src/main/java/com/commonlibrary/http/PostResponse.java
+++ commonlibrary/src/main/java/com/commonlibrary/http/PostResponse.java
@@ -8,60 +8,62 @@ import java.util.Map;
  */
 public class PostResponse {
 
-    private int code;
-    private String message;
-    private byte[] data;
-    private Map<String, String> responseHeaders;
+  private int code;
+  private String message;
+  private byte[] data;
+  private Map<String, String> responseHeaders;
 
-    public void setCode(int code) {
-        this.code = code;
+  public void setCode(int code) {
+    this.code = code;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setData(byte[] data) {
+    this.data = data;
+  }
+
+  public byte[] getData() {
+    return data;
+  }
+
+  public String getStringData() {
+    return new String(data);
+  }
+
+  public void setResponseHeaders(Map<String, String> responseHeaders) {
+    this.responseHeaders = responseHeaders;
+  }
+
+  public Map<String, String> getResponseHeaders() {
+    return responseHeaders;
+  }
+
+  public void putResponseHeader(String key, String value) {
+    if (responseHeaders == null) responseHeaders = new HashMap<>();
+
+    responseHeaders.put(key, value);
+  }
+
+  public String getHeader(String key) {
+    if (responseHeaders != null) {
+      return responseHeaders.get(key);
     }
 
-    public int getCode() {
-        return code;
-    }
+    return null;
+  }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setData(byte[] data) {
-        this.data = data;
-    }
-
-    public byte[] getData() {
-        return data;
-    }
-
-    public void setResponseHeaders(Map<String, String> responseHeaders) {
-        this.responseHeaders = responseHeaders;
-    }
-
-    public Map<String, String> getResponseHeaders() {
-        return responseHeaders;
-    }
-
-    public void putResponseHeader(String key, String value) {
-        if (responseHeaders == null)
-            responseHeaders = new HashMap<>();
-
-        responseHeaders.put(key, value);
-    }
-
-    public String getHeader(String key){
-        if(responseHeaders!=null){
-            return responseHeaders.get(key);
-        }
-
-        return null;
-    }
-
-    public boolean isSuccess(){
-        return code >= 200 && code < 300;
-    }
-
+  public boolean isSuccess() {
+    return code >= 200 && code < 300;
+  }
 }
