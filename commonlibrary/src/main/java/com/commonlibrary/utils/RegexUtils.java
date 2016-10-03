46535103fbbb68b428a60b6ab852cf9442233a88
hzf <huangzefeng30@gmail.com>
2016-10-03 20:03:03 +0800

    正则相关工具

---
+++ commonlibrary/src/main/java/com/commonlibrary/utils/RegexUtils.java
@@ -0,0 +1,28 @@
+package com.commonlibrary.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by huangzefeng on 2016/10/3.
+ */
+
+public class RegexUtils {
+
+  private final static String CHAR_OR_NUMBER_LIMIT = "[0-9A-Za-z]\\d*";
+
+  private final static Pattern CHAR_OR_NUMBER_PATTERN;
+
+  static {
+    CHAR_OR_NUMBER_PATTERN = Pattern.compile(CHAR_OR_NUMBER_LIMIT);
+  }
+
+  public static boolean isOnlyCharOrNumber(String text){
+    if(text == null)
+      return false;
+
+    Matcher matcher = CHAR_OR_NUMBER_PATTERN.matcher(text);
+    return matcher.matches();
+  }
+
+}
